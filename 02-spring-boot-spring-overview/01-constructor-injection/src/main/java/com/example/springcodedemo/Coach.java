aa5d019995cb432b7848872ede79cddd661d8fae
Akshita <akshita10656jain@gmail.com>
2025-12-04 10:28:42 +0530

    REST API security

--- 02-spring-boot-spring-overview/01-constructor-injection/src/main/java/com/example/springcodedemo/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.example.springcodedemo;
-
-public interface Coach {
-    String getDailyWorkout();
-
-}
--- 02-spring-boot-spring-overview/02--component-scanning/src/main/java/com/example/springcodedemo/common/Coach.java
+++
@@ -1,6 +0,0 @@
-package com.example.springcodedemo.common;
-
-public interface Coach {
-    String getDailyWorkout();
-
-}
